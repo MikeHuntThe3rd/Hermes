9dbb6978e16a647bbb78bf356a977ca9d0cc4a04
MikeHuntThe3rd <103815901+MikeHuntThe3rd@users.noreply.github.com>
2026-07-16 18:14:47 +0200

    db rewrite

--- src/handlers/post.rs
+++ src/handlers/post.rs
@@ -3,7 +3,6 @@ use axum::{Json, http::StatusCode};
 use crate::types::*;
 use crate::handlers::get_db_interface;
 
-#[axum::debug_handler]
 pub async fn add_user(Json(data): Json<UninitializedUser>) -> Result<Json<User>, StatusCode> {
     let inf = get_db_interface().await;
 
